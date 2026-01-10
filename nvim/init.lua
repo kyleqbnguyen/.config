ba412be82f8a73a6ce99e617f2bfda13d1c9642d
Kyle Nguyen <kyleqbnguyen@gmail.com>
2026-01-10 12:35:40 -0800

    revert: reconfigure hyprland controls for a normal keyboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,6 +15,9 @@ vim.o.termguicolors = true
 
 -- Text
 vim.o.tabstop = 2
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
 vim.o.wrap = false
 vim.o.smartindent = true
 vim.o.incsearch = true
