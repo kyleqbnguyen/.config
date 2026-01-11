bc673f419b9c3a6509c5fdb81a6a22acf4b0f80c
Kyle Nguyen <kyleqbnguyen@gmail.com>
2026-01-11 13:47:47 -0800

    fix: harpoon compatability

--- nvim/init.lua
+++ nvim/init.lua
@@ -191,10 +191,10 @@ vim.keymap.set("n", "<leader>a", mark.add_file)
 
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-q>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-w>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-f>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-p>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<A-n>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<A-e>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<A-i>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<A-o>", function() ui.nav_file(4) end)
 
 --------------------------------------------------------------------------------
 --- Markdown
